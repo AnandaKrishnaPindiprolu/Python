d3fe2fadd42ee29786f9b105e3bb0935ea356c76
AnandaKrishnaPindiprolu <anandkrishnapindiprolu@gmail.com>
2024-08-24 11:18:21 +0530

    added another indenttaion code file

---
+++ Indentation/indentation_withcondition.py
@@ -0,0 +1,9 @@
+#indentation with conditions
+'''
+here conditions like if and else will have some indentation and if any statements inside a condtion then the statments will have a space
+'''
+
+if(4<1):
+    print("the highest number is",4)
+else:
+    print("the lowest number is",1)
