2beabc732288873d67716b4384b9709813f4d7f0
AnandaKrishnaPindiprolu <anandkrishnapindiprolu@gmail.com>
2024-08-24 10:54:27 +0530

    indentation code

---
+++ Indentation/indentation.py
@@ -0,0 +1,13 @@
+#indentation with statements
+'''
+here we have to print statements and both should be in same line without spaces if spaces then we will face indentation issues eg
+proper indentation code
+'''
+print("first statement")
+print("second line")
+
+'''
+improper indentation
+print("first statement")
+ print("second line")
+'''
